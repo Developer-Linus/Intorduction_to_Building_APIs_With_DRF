ed0214eb356d62134fe05bc8c9eae6a1d4ebbfa7
Developer-Linus <linusmksu30@gmail.com>
2025-03-03 10:02:39 +0300

    completed reading on reading serializers and querysets in DRF

--- my_app/serializers.py
+++ my_app/serializers.py
@@ -3,7 +3,11 @@ from rest_framework import serializers
 from .models import Book
 
 class BookSerializer(serializers.ModelSerializer):
+    days_since_created = serializers.SerializerMethodField()
     class Meta:
         model = Book
         fields = '__all__'
-        
+    def get_days_since_created(self, obj):
+        from datetime import datetime, timezone
+        return(datetime.now(timezone.utc)-obj.created_at).days
+    
